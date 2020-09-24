c62702eb0a3652a7ae30d41f4d60e41e04f02fe3
Miguel Angel Dulcey Diaz <mpgigat@gmail.com>
2020-09-24 13:05:26 -0500

    fin seccion

---
+++ android/app/src/main/kotlin/com/nsyzsm5000/ej01_socket_app_realtime_bandnameapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nsyzsm5000.ej01_socket_app_realtime_bandnameapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,123 @@
+import 'dart:io';
+
+import 'package:ej01_socket_app_realtime_bandnameapp/models/band.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Band> bands = [
+    Band(id: '1', name: 'Metallica', votes: 5),
+    Band(id: '2', name: 'Queen', votes: 22),
+    Band(id: '3', name: 'Mana', votes: 25),
+    Band(id: '4', name: 'Bon Jovi', votes: 15)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BandNames',
+          style: TextStyle(color: Colors.black87),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1,
+      ),
+      body: ListView.builder(
+          itemCount: bands.length,
+          itemBuilder: (context, index) {
+            return _bandTile(bands[index]);
+          }),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add), elevation: 1, onPressed: addNewBand),
+    );
+  }
+
+  Widget _bandTile(Band band) {
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction){
+        //llamar borrado server
+      },
+      background: Container(
+        padding: EdgeInsets.only(left: 8),
+        color: Colors.red,
+        child: Align(alignment: Alignment.centerLeft,
+        child: Text('Delete Band',style: TextStyle(color: Colors.white),),
+        ),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(band.name.substring(0, 2)),
+          backgroundColor: Colors.blue[100],
+        ),
+        title: Text(band.name),
+        trailing: Text(
+          '${band.votes}',
+          style: TextStyle(fontSize: 20),
+        ),
+        onTap: () {
+          print(band.name);
+        },
+      ),
+    );
+  }
+
+  addNewBand() {
+    final textController = TextEditingController();
+
+    // if (Platform.isAndroid) {
+    //   showDialog(
+    //       context: context,
+    //       builder: (context) {
+    //         return AlertDialog(
+    //           title: Text('New band Name'),
+    //           content: TextField(
+    //             controller: textController,
+    //           ),
+    //           actions: [
+    //             MaterialButton(
+    //                 child: Text('Add'),
+    //                 elevation: 5,
+    //                 textColor: Colors.blue,
+    //                 onPressed: () => addBandToList(textController.text))
+    //           ],
+    //         );
+    //       });
+    // }
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text('New band Name'),
+            content: CupertinoTextField(
+              controller: textController,
+            ),
+            actions: [
+              CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: Text('Add'),
+                  onPressed: () => addBandToList(textController.text)),
+              CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: Text('Dismiss'),
+                  onPressed: () => Navigator.pop(context))
+            ],
+          );
+        });
+  }
+
+  void addBandToList(String name) {
+    if (name.length > 1) {
+      this.bands.add(Band(id: DateTime.now().toString(), name: name, votes: 0));
+      setState(() {});
+    }
+    Navigator.pop(context);
+  }
+}
